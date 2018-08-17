8d1291d759d19cd9c572896ef289c58e6f0e04c8
David Jungst <djungst@gmail.com>
2018-08-17 14:35:36 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# About
+
+This package fits a very specific use case.  It's purpose is to keep help keep track of new tokens everytime a request is made.  It works by capturing the axios response and hot swapping tokens if new tokens have come back from the server.  On an authenticated request tokens are destroyed on signout and tokens are rehydrated on a validation event.
+
+ * You are using devise token authentiation
+ * You are using axios
+ 
+# Installation
+```npm install devise-axios```
+```
+import { initMiddleware } from './utils/api';
+
+initMiddleware()
+```
+
+You can also change the default auth url and options
+
+```
+  //defaults
+  //{
+  //  authPrefix: '/api/auth',
+  //  signOut: '/sign_out',
+  //  validate: '/validate_token',
+  //}
+
+  const options = { authPrefix: '/auth' }
+  initMiddleware(options)
+```
+
---
+++ src/index.js
@@ -0,0 +1,75 @@
+const HEADERS = ['access-token', 'token-type', 'client', 'expiry', 'uid']
+
+const setTokens = (headers) => {
+  for (let token of HEADERS) {
+    console.log('in loop')
+    axios.defaults.headers.common[token] = headers[token]
+    localStorage.setItem(token, headers[token])
+  }
+  console.log('out of loop')
+}
+
+export const getTokens = () => {
+  let headers = {}
+  for (let token of HEADERS) {
+    const t = localStorage.getItem(token)
+    headers[token] = t
+  }
+
+  return headers
+}
+
+const clearTokens = () => {
+  for (let token of HEADERS) {
+    localStorage.removeItem(token)
+  }
+}
+
+export const initMiddleware = (options = {}) => {
+  const defaults = {
+    authPrefix: '/api/auth',
+    signOut: '/sign_out',
+    validate: '/validate_token',
+  }
+
+  const settings = { ...defaults, ...options }
+
+  axios.interceptors.response.use( (response) => {
+    const { headers } = response
+    const oldHeaders = axios.defaults.headers.common
+    if (headers['access-token'] && headers['access-token'] !== oldHeaders['access-token'])
+      setTokens(headers)
+    return response;
+  }, (error) => {
+    return Promise.reject(error);
+  });
+
+  axios.interceptors.request.use( (request) => {
+    const { url } = request
+    const { authPrefix, signOut, validate } = settings
+    const authRegex = new RegExp(authPrefix)
+    if (authRegex.test(request.url)) {
+      const path = url.split(authPrefix)[1]
+      switch(path) {
+        case validate:
+          const headers = getTokens()
+          request = {...request, ...headers }
+          const common = {...request.headers.common, ...headers}
+          axios.defaults.headers.common = common
+          request.headers.common = common
+          break
+        case signOut:
+          clearTokens()
+          break
+        default:
+          return request
+      }
+    }
+
+    return request
+  }, (error) => {
+    return Promise.reject(error)
+  })
+}
+
+
