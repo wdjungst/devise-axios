c1b23d55653bd7e582695a4f414c2b134359dd8b
David Jungst <djungst@gmail.com>
2018-09-20 20:03:01 -0600

    plugins

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -34,6 +34,7 @@ module.exports = {
         loader: 'babel-loader',
         options: {
           presets: ["@babel/preset-env"],
+          plugins: [
             ["@babel/plugin-transform-runtime", {
               "corejs": false,
               "helpers": true,
